532d10120ef4075904d903b8e58166766df7b2e5
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2013-03-09 17:32:06 -0600

    Added Screen VC and external screen stuff

---
+++ FutureStop/Rene/FSReneContainerViewController.m
@@ -0,0 +1,18 @@
+//
+//  FSReneContainerViewController.m
+//  FutureStop
+//
+//  Created by Rene Cacheaux on 3/9/13.
+//  Copyright (c) 2013 Mutual Mobile SXSW Creatathon. All rights reserved.
+//
+
+#import "FSReneContainerViewController.h"
+
+@implementation FSReneContainerViewController
+
+- (void)loadView {
+  self.view = [[UIView alloc] init];
+  self.view.backgroundColor = [UIColor orangeColor];
+}
+
+@end
